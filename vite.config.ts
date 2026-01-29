b181bed3d97ba07417601c1cd2a70604204cc321
thetmyoekhaing <thetmyoekhaing.npi@gmail.com>
2026-01-29 15:52:18 +0630

    [UPDATE] vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/grindmal-web/",
+  // base: "/grindmal-web/",
   server: {
     host: "::",
     port: 8080,
